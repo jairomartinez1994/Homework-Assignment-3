b18056467292891479a0e0f594a4532c78e09e74
jairomartinez1994 <61437470+jairomartinez1994@users.noreply.github.com>
2020-05-13 15:17:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,46 @@
-//assigning variable
-var lowerchar1 = "abcdefghijklmnopqrstuvwxyz";
-var upperchar2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = "0123456789";
-var symbols = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-
-// Assignment Code
-var charNumber = document.getElementById("characternumber");//numbers will randomly be chosen
-var uppercase = document.getElementById("uppercase");// Will choose any uppercase letter
-var numInput = document.getElementById("number"); // Will choose any available number
-var symbInput = document.getElementById("symbol"); // Will choose any available symbol
-var passwordText = document.querySelector("#password"); // Will Generate password
-var generateBtn = document.querySelector("#generate"); // Will be the button to generate a password
-
-// Write password to the #password input
-
-function writePassword() {
-  var characters = lowerchar1;
-  var lengthchar = charNumber.value; // the length of the number will be given the value by .value
+var char1 = "abcdefghijklmnopqrstuvwxyz";
+var char2="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var num = "0123456789";
+var sym = "~`!@#$%^&*()_-+={[}]|\:;\"'<,>.?/";
 
 
-  if(numInput.radio==true){              // When number is chosen
-    characters+=numbers;            // then will focus on the character and number chosen
-                                    // then characters value will be the number and character
-                                    // When the password is selected
-                                    // then the symbols will be randomly chosen
+var charNumber = document.getElementById("charNum");
+var upperCase=document.getElementById("upperCase");
+var numInput = document.getElementById("num");
+var symbInput = document.getElementById("symb");
+var passwordText = document.querySelector("#password");
+var generateBtn = document.querySelector("#generate");
+
+
+
+function writePassword(){
+  var characters = char1;
+  var lengthchar = charNumber.value;
+  
+  if(numInput.checked==true){    
+    characters+=num;            
+                                
   }
-
-  if(symbInput.radio==true){
-    characters+=symbols;
+  if(symbInput.checked==true){
+    characters+=sym;
   
 }
-  
-  if(uppercase.radio==true){
-characters+=upperchar2;
+if(upperCase.checked==true){
+  characters+=char2;
 
 }
-  var password = generatePassword(characters, lengthchar);
-  passwordText.value = password;
-  
-  function generatePassword (characters, lengthchar){
-    var localpass = "";
-    for (var i = 0; i < lengthchar; i++){
-      localpass = localpass + characters.charAt(Math.floor(Math.random() * characters.length));
-      
-    }
-    return localpass
+var password = generatePassword(lengthchar, characters);
+passwordText.value = password;
+}
+
+function generatePassword(lengthchar, characters) {
+  var localPass = "";
+  for (var i = 0; i < lengthchar; i++) {
+    localPass = localPass + characters.charAt(Math.floor(Math.random() * characters.length));
+
   }
+  return localPass;
+}
 
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
